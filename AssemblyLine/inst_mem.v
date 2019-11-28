5bcc5fc3c57d5bf9277716b1c73785483ab32ee1
Venci Freeman <37969638+VenciFreeman@users.noreply.github.com>
2019-11-28 21:56:04 +0800

    Init mem.

---
+++ AssemblyLine/inst_mem.v
@@ -0,0 +1,21 @@
+module inst_mem(
+
+	input wire					ce,
+	input wire[31:0]			addr,
+	output reg[31:0]			inst
+	
+);
+
+	reg[31:0]  inst_memory[0:1000];
+
+	initial $readmemb ( "./machinecode.txt", inst_memory );
+
+	always @ * begin
+		if (ce == 1'b0) begin
+			inst <= 32'b0;
+	    end else begin
+		    inst <= inst_memory [addr[31:2]];
+		end
+	end
+
+endmodule
