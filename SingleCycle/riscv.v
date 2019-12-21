3905340b9a7874263e05ccec510d7347afd5c990
Venci Freeman <37969638+VenciFreeman@users.noreply.github.com>
2019-12-21 17:19:46 +0800

    Update riscv.v

--- SingleCycle/riscv.v
+++ SingleCycle/riscv.v
@@ -15,23 +15,23 @@
  *
  */
 
-`include "data_mem.v"
-`include "ex.v"
-`include "id.v"
-`include "if.v"
-`include "inst_mem.v"
-`include "mem.v"
-`include "register.v"
+`include "C:/Users/Venci/Documents/GitHub/RISC-V_CPU/SingleCycle/data_mem.v"
+`include "C:/Users/Venci/Documents/GitHub/RISC-V_CPU/SingleCycle/ex.v"
+`include "C:/Users/Venci/Documents/GitHub/RISC-V_CPU/SingleCycle/id.v"
+`include "C:/Users/Venci/Documents/GitHub/RISC-V_CPU/SingleCycle/if.v"
+`include "C:/Users/Venci/Documents/GitHub/RISC-V_CPU/SingleCycle/inst_mem.v"
+`include "C:/Users/Venci/Documents/GitHub/RISC-V_CPU/SingleCycle/mem.v"
+`include "C:/Users/Venci/Documents/GitHub/RISC-V_CPU/SingleCycle/register.v"
 
 module riscv(
 
 	input wire				 clk,
-	input wire				 rst,        // high is reset
+	input wire				 rst,         // high is reset
 	
     // inst_mem
-	input wire[31:0]         inst_i,
-	output wire[31:0]        inst_addr_o,
-	output wire              inst_ce_o,	
+	input wire[31:0]         inst_i,	  // instruction
+	output wire[31:0]        inst_addr_o, // instruction address
+	output wire              inst_ce_o,	  // chip select signal
 
     // data_mem
 	input wire[31:0]         data_i,      // load data from data_mem
