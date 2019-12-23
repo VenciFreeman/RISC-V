3dc35e2b6f43deb753969cea9e13e3fec6d1d394
Venci Freeman <37969638+VenciFreeman@users.noreply.github.com>
2019-12-24 03:41:09 +0800

    Update id.v

--- SingleCycle/id.v
+++ SingleCycle/id.v
@@ -20,69 +20,62 @@
 
  * History:
  * - 19/12/19: Edit the RISC-V instructions;
- * - 19/12/23: Add control module.
+ * - 19/12/23: Add control module;
+ * - 19/12/24: Change the structure.
 
  * Notes:
  *
+ ***************************************************************************************
+ * Inst * ALUSrc * MemToReg * RegWrite * MemRead * MemWrite * Branch * ALUOp1 * ALUOp0 *
+ ***************************************************************************************
+ *   R  *    0   *     0    *     1    *    0    *     0    *    0   *    1   *    0   *
+ ***************************************************************************************
+ *  lw  *    1   *     1    *     1    *    1    *     0    *    0   *    0   *    0   *
+ ***************************************************************************************
+ *  sw  *    1   *     x    *     0    *    0    *     1    *    0   *    0   *    0   *
+ ***************************************************************************************
+ * beq  *    0   *     x    *     0    *    0    *     0    *    1   *    0   *    1   *
+ ***************************************************************************************
+ *
  */
-
-module ID(
-
-        input   [6:0] opcode,
-        input   [2:0] funct3,
-        input   [6:0] funct7,
-        output  [5:0] ALUop_o
-
-    );
-    
-    reg [3:0] ALUop;
-
-    assign ALUop_o = ALUop;
-
-/*
-* This always part controls the signal ALUop.
-*/   
-always @ (*) begin
     casex ({opcode, funct3, funct7})
-        17'b01100110000000000: ALUop <= 6'b000001;  // add
-        17'b01100110000100000: ALUop <= 6'b000010;  // sub
-        17'b01100110010000000: ALUop <= 6'b000011;  // sll
-        17'b1101111xxxxxxxxxx: ALUop <= 6'b000100;  // jal
-        17'b0010011000xxxxxxx: ALUop <= 6'b000101;  // addi
-        17'b01100111110000000: ALUop <= 6'b000110;  // and
-        17'b01100111100000000: ALUop <= 6'b000111;  // or
-        17'b01100111000000000: ALUop <= 6'b001000;  // xor
-        17'b1100111100xxxxxxx: ALUop <= 6'b001001;  // blt
-        17'b1100111000xxxxxxx: ALUop <= 6'b001010;  // beq
-        17'b01100111010000000: ALUop <= 6'b001011;  // srl
-        17'b0000011010xxxxxxx: ALUop <= 6'b001100;  // lw
-        17'b0100011010xxxxxxx: ALUop <= 6'b001101;  // sw
-        default:               ALUop <= 6'bzzzzzz;
-    endcase
-end
+        17'b0110011 000 0000000: ALUop <= 6'b000001;  // add  R
+        17'b0110011 000 0100000: ALUop <= 6'b000010;  // sub  R
+        17'b0110011 001 0000000: ALUop <= 6'b000011;  // sll  R
+        17'b0110011 101 0000000: ALUop <= 6'b001011;  // srl  R
+        17'b0110011 111 0000000: ALUop <= 6'b000110;  // and  R
+        17'b0110011 110 0000000: ALUop <= 6'b000111;  // or   R
+        17'b0110011 100 0000000: ALUop <= 6'b001000;  // xor  R
+        17'b1101111 xxx xxxxxxx: ALUop <= 6'b000100;  // jal  J
+        17'b1100111 100 xxxxxxx: ALUop <= 6'b001001;  // blt  B
+        17'b1100111 000 xxxxxxx: ALUop <= 6'b001010;  // beq  B
+        17'b0010011 000 xxxxxxx: ALUop <= 6'b000101;  // addi I
+        17'b0000011 010 xxxxxxx: ALUop <= 6'b001100;  // lw   I
+        17'b0100011 010 xxxxxxx: ALUop <= 6'b001101;  // sw   S
+        default:                 ALUop <= 6'bzzzzzz;
 
-endmodule
 
 module Control(
 
-    input [5:0] Opcode,
-    input [5:0] Funct,
+    input [6:0] Opcode,
+    input [2:0] funct3,
+    input [6:0] funct7,
     output      RegDst,
     output      RegWrite,
     output      MemRead,
     output      MemWrite,
     output      MemToReg,
-    output      NeedZEXT,
     output      ALUsrc,
-    output[3:0] ALUop,
+    output[6:0] ALUop,
     output      Branch,
     output[1:0] Jump,
     output      Link,
-    output[1:0] StackOp
+    output      ALUOp1,
+    output      ALUOp0
 
 );
 
-    reg regDst, regWrite, memRead, memWrite, memToReg, needZEXT, aluSrc, branch, link;
+    reg regDst, regWrite, memRead, memWrite, memToReg, needZEXT, aluSrc, branch, link, ALUOp1, ALUOp0;
     reg [3:0] aluOp;
     reg [1:0] jump, stackOp;
 
@@ -99,136 +92,145 @@ module Control(
     assign Link = link;
     assign StackOp = stackOp;
 
+/*
+ * This always part controls the signal regDst.
+ */
 always @ (Opcode or Funct) begin
     case (Opcode)
-        6'b000000: regDst <= 1;  // R-type, use Rd
-        6'b001000: regDst <= 0;  // addi, use Rt   
-        6'b001100: regDst <= 0;  // andi, use Rt
-        6'b000100: regDst <= 0;  // beq  
-        6'b000011: regDst <= 0;  // jal
-        6'b100011: regDst <= 0;  // lw
-        6'b101011: regDst <= 0;  // sw
+        7'b0000011: regDst <= 0;  // I-type, lw.
+        7'b0010011: regDst <= 0;  // I-type, addi.
+        7'b0100011: regDst <= 0;  // S-type, sw.
+        7'b0110011: regDst <= 1;  // R-type, including add, sub, sll, srl, and, or & xor.
+        7'b1100011: regDst <= 0;  // B-type, including beq & blt.  
+        7'b1101111: regDst <= 0;  // J-type, including jal.
     endcase
 end
 
+/*
+ * This always part controls the signal regWrite.
+ */
 always @ (Opcode or Funct) begin
     case (Opcode)
-        6'b000000: regWrite <= 1;  // R-type, use Rd
-        6'b001000: regWrite <= 1;  // addi, use Rt   
-        6'b000100: regWrite <= 0;  // beq  
-        6'b000011: regWrite <= 0;  // jal
-        6'b100011: regWrite <= 1;  // lw
-        6'b101011: regWrite <= 0;  // sw
+        7'b0000011: regWrite <= 1;  // I-type, lw.
+        7'b0010011: regWrite <= 1;  // I-type, addi.
+        7'b0100011: regWrite <= 0;  // S-type, sw.
+        7'b0110011: regWrite <= 1;  // R-type, including add, sub, sll, srl, and, or & xor.
+        7'b1100011: regWrite <= 0;  // B-type, including beq & blt.  
+        7'b1101111: regWrite <= 0;  // J-type, including jal.
     endcase
 end
 
+/*
+ * This always part controls the signal memRead.
+ */
 always @ (Opcode or Funct) begin
     case (Opcode)
-        6'b000000: memRead <= 0;  // R-type, use Rd
-        6'b001000: memRead <= 0;  // addi, use Rt   
-        6'b000100: memRead <= 0;  // beq  
-        6'b000011: memRead <= 0;  // jal
-        6'b100011: memRead <= 1;  // lw
-        6'b101011: memRead <= 0;  // sw
+        7'b0000011: memRead <= 1;  // I-type, lw.
+        7'b0010011: memRead <= 0;  // I-type, addi.
+        7'b0100011: memRead <= 0;  // S-type, sw.
+        7'b0110011: memRead <= 0;  // R-type, including add, sub, sll, srl, and, or & xor.
+        7'b1100011: memRead <= 0;  // B-type, including beq & blt.  
+        7'b1101111: memRead <= 0;  // J-type, including jal.
     endcase
 end
 
+/*
+ * This always part controls the signal memWrite.
+ */
 always @ (Opcode or Funct) begin
     case (Opcode)
-        6'b000000: memWrite <= 0;  // R-type, use Rd
-        6'b001000: memWrite <= 0;  // addi, use Rt   
-        6'b000100: memWrite <= 0;  // beq  
-        6'b000011: memWrite <= 0;  // jal
-        6'b100011: memWrite <= 0;  // lw
-        6'b101011: memWrite <= 1;  // sw
+        7'b0000011: memWrite <= 0;  // I-type, lw.
+        7'b0010011: memWrite <= 0;  // I-type, addi.
+        7'b0100011: memWrite <= 1;  // S-type, sw.
+        7'b0110011: memWrite <= 0;  // R-type, including add, sub, sll, srl, and, or & xor.
+        7'b1100011: memWrite <= 0;  // B-type, including beq & blt.  
+        7'b1101111: memWrite <= 0;  // J-type, including jal.
     endcase
 end
 
+/*
+ * This always part controls the signal memToReg.
+ */
 always @ (Opcode or Funct) begin
     case (Opcode)
-        6'b000000: memToReg <= 0;  // R-type, use Rd
-        6'b001000: memToReg <= 0;  // addi, use Rt   
-        6'b000100: memToReg <= 0;  // beq  
-        6'b000011: memToReg <= 0;  // jal
-        6'b100011: memToReg <= 1;  // lw
-        6'b101011: memToReg <= 0;  // sw
+        7'b0000011: memToReg <= 1;  // I-type, lw.
+        7'b0010011: memToReg <= 0;  // I-type, addi.
+        7'b0100011: memToReg <= 0;  // S-type, sw.
+        7'b0110011: memToReg <= 0;  // R-type, including add, sub, sll, srl, and, or & xor.
+        7'b1100011: memToReg <= 0;  // B-type, including beq & blt.  
+        7'b1101111: memToReg <= 0;  // J-type, including jal.
     endcase
 end
 
+/*
+ * This always part controls the signal aluSrc.
+ */
 always @ (Opcode or Funct) begin
     case (Opcode)
-        6'b000000: needZEXT <= 0;  // R-type, use Rd
-        6'b001000: needZEXT <= 0;  // addi, use Rt   
-        6'b000100: needZEXT <= 0;  // beq  
-        6'b000011: needZEXT <= 0;  // jal
-        6'b100011: needZEXT <= 0;  // lw
-        6'b101011: needZEXT <= 0;  // sw
+        7'b0000011: aluSrc <= 1;  // I-type, lw.
+        7'b0010011: aluSrc <= 1;  // I-type, addi.
+        7'b0100011: aluSrc <= 1;  // S-type, sw.
+        7'b0110011: aluSrc <= 0;  // R-type, including add, sub, sll, srl, and, or & xor.
+        7'b1100011: aluSrc <= 0;  // B-type, including beq & blt.  
+        7'b1101111: aluSrc <= 1;  // J-type, including jal.
     endcase
 end
 
+/*
+ * This always part controls the signal aluOp.
+ */
 always @ (Opcode or Funct) begin
     case (Opcode)
-        6'b000000: aluSrc <= 0;  // R-type, use Rd
-        6'b001000: aluSrc <= 1;  // addi, use Rt   
-        6'b000100: aluSrc <= 0;  // beq  
-        6'b000011: aluSrc <= 1;  // jal
-        6'b100011: aluSrc <= 1;  // lw
-        6'b101011: aluSrc <= 1;  // sw
+        7'b0000011: aluOp <= 6'b;  // I-type, lw.
+        7'b0010011: aluOp <= 6'b;  // I-type, addi.
+        7'b0100011: aluOp <= 6'b;  // S-type, sw.
+        7'b0110011: aluOp <= 6'b;  // R-type, including add, sub, sll, srl, and, or & xor.
+        7'b1100011: aluOp <= 6'b;  // B-type, including beq & blt.  
+        7'b1101111: aluOp <= 6'b000100;  // J-type, including jal.
+
+        // I don't know how to decide ALUOp... for R-type, maybe I can use Funct3/7?
     endcase
 end
 
+/*
+ * This always part controls the signal branch.
+ */
 always @ (Opcode or Funct) begin
     case (Opcode)
-        6'b000000: aluOp <= Funct[3:0];  // R-type, use Rd
-        6'b001000: aluOp <= 4'b0010;  // addi, use Rt   
-        6'b000100: aluOp <= 4'b0011;  // beq  
-        6'b000011: aluOp <= 4'b0000;  // jal
-        6'b100011: aluOp <= 4'b0010;  // lw
-        6'b101011: aluOp <= 4'b0010;  // sw
+        7'b0000011: branch <= 0;  // I-type, lw.
+        7'b0010011: branch <= 0;  // I-type, addi.
+        7'b0100011: branch <= 0;  // S-type, sw.
+        7'b0110011: branch <= 0;  // R-type, including add, sub, sll, srl, and, or & xor.
+        7'b1100011: branch <= 1;  // B-type, including beq & blt.  
+        7'b1101111: branch <= 0;  // J-type, including jal.
     endcase
 end
 
+/*
+ * This always part controls the signal jump.
+ */
 always @ (Opcode or Funct) begin
     case (Opcode)
-        6'b000000: branch <= 0;  // R-type, use Rd
-        6'b001000: branch <= 0;  // addi, use Rt   
-        6'b000100: branch <= 1;  // beq  
-        6'b000011: branch <= 0;  // jal
-        6'b100011: branch <= 0;  // lw
-        6'b101011: branch <= 0;  // sw
+        7'b0000011: jump <= 2'b00;  // I-type, lw.
+        7'b0010011: jump <= 2'b00;  // I-type, addi.
+        7'b0100011: jump <= 2'b00;  // S-type, sw.
+        7'b0110011: jump <= 2'b00;  // R-type, including add, sub, sll, srl, and, or & xor.
+        7'b1100011: jump <= 2'b00;  // B-type, including beq & blt.  
+        7'b1101111: jump <= 2'b01;  // J-type, including jal.
     endcase
 end
 
+/*
+ * This always part controls the signal link.
+ */
 always @ (Opcode or Funct) begin
     case (Opcode)
-        6'b000000: jump <= 2'b00;  // R-type, use Rd
-        6'b001000: jump <= 2'b00;  // addi, use Rt   
-        6'b000100: jump <= 2'b00;  // beq  
-        6'b000011: jump <= 2'b01;  // jal
-        6'b100011: jump <= 2'b00;  // lw
-        6'b101011: jump <= 2'b00;  // sw
-    endcase
-end
-
-always @ (Opcode or Funct) begin
-    case (Opcode)
-        6'b000000: link <= 0;  // R-type, use Rd
-        6'b001000: link <= 0;  // addi, use Rt   
-        6'b000100: link <= 0;  // beq  
-        6'b000011: link <= 1;  // jal
-        6'b100011: link <= 0;  // lw
-        6'b101011: link <= 0;  // sw
-    endcase
-end
-
-always @ (Opcode or Funct) begin
-    case (Opcode)
-        6'b000000: stackOp <= 2'b00;  // R-type, use Rd
-        6'b001000: stackOp <= 2'b00;  // addi, use Rt   
-        6'b000100: stackOp <= 2'b00;  // beq  
-        6'b000011: stackOp <= 2'b00;  // jal
-        6'b100011: stackOp <= 2'b00;  // lw
-        6'b101011: stackOp <= 2'b00;  // sw
+        7'b0000011: link <= 0;  // I-type, lw.
+        7'b0010011: link <= 0;  // I-type, addi.
+        7'b0100011: link <= 0;  // S-type, sw.
+        7'b0110011: link <= 0;  // R-type, including add, sub, sll, srl, and, or & xor.
+        7'b1100011: link <= 0;  // B-type, including beq & blt.  
+        7'b1101111: lunk <= 1;  // J-type, including jal.
     endcase
 end
 
