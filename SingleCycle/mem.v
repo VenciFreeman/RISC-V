6d24ab9ae0734801b676c6999bd763a3146cf3e5
Venci Freeman <37969638+VenciFreeman@users.noreply.github.com>
2019-12-26 21:50:56 +0800

    Finished mem.v

--- SingleCycle/mem.v
+++ SingleCycle/mem.v
@@ -7,98 +7,127 @@
  * This file controls the read and write about data_mem.
 
  * Details:
- * - Read 32bit data from data_mem if instruction is lw;
- * - Write 32bit data into data_mem if instruction is sw;
+ * - Read 32 bit data from data_mem if instruction is lw;
+ * - Write 32 bit data into data_mem if instruction is sw;
  * - Do no operation if there is other instruction.
 
  * History:
  * - 19/12/05: Create this file;
- * - 19/12/23: Add PeriphMem and StackMem module.
+ * - 19/12/23: Add module;
+ * - 19/12/26: Update module. (I think it's finished).
 
  * Notes:
  *
  */
 
-module PeriphMem(
+module mem (
 
-    input   	 clk,
-    input [31:0] address,
-    input [31:0] writeData,
-    input        memRead,
-    input        memWrite,
-    output[31:0] readData
-
-    );
-
-    reg [31:0] mem [1023:0];
-    reg [31:0] ReadData;
-    assign readData = ReadData;
-
-/*
- * This always part controls the signal ReadData.
- */    
-always @ (address or memRead or clk) begin
-	if (address >= 32'h00002000 && address < 32'h00003000 && memRead)
-			ReadData <= mem[((address & 32'h0000FFFF) - 32'h00002000) >> 2];
-end
-
-/*
- * This always part controls the reg mem.
- */ 
-always @ (negedge clk) begin
-	if (address >= 32'h00002000 && address < 32'h00003000 && memWrite)
-			mem[((address & 32'h0000FFFF) - 32'h00002000) >> 2] <= writeData;
-end
-
-endmodule
-
-module StackMem (
-
-    input [31:0] Address,
-    input [31:0] StackWRdata,
-    input [31:0] NewSP,
-    input        UpdateSP,
-    input        WE,
-    input [1:0]  StackOp,
-    input        clk,
-    input        rst,
-    output[31:0] StackRDdata,
-    output[31:0] SP
+    input   wire        rst,
+    input   wire        WriteReg_i,
+    input   wire[4:0]   WriteData_i,
+    input   wire[4:0]   ALUop_i,
+    input   wire[31:0]  WriteDataAddr_i,
+    input   wire[31:0]  MemAddr_i,
+    input   wire[31:0]  Reg_i,
+    input   wire[31:0]  MemData_i,
+    output  wire        MemWE_o,
+    output  reg         WriteReg_o,
+    output  reg         MemCE_o,
+    output  reg [4:0]   WriteData_o,
+    output  reg [31:0]  WriteDataAddr_o,
+    output  reg [31:0]  MemAddr_o,
+    output  reg [31:0]  MemData_o
 
 );
 
-    reg [31:0] sp;
-	reg [31:0] readData;
-	reg [31:0] mem [1023:0];
-    wire[31:0] sp_minus_4;
+    reg mem_we;
+    assign MemWE_o = mem_we;
 
-    assign sp_minus_4 = sp - 4;
-    assign SP = sp;
-    assign StackRDdata = readData;
-
+/*
+ * This always part controls the signal WriteData_o.
+ */ 
 always @ (*) begin
-	if (Address >= 32'h00003000 && Address < 32'h00004000)
-		readData <= mem[(sp & 32'h00000FFF) >> 2];
-	else if (StackOp == 2'b11) // pop 
-		readData <= mem[(sp & 32'h00000FFF) >> 2];
+    if (rst)
+        WriteData_o = 5'b0;
+    else
+        WriteData_o = WriteData_i;
 end
 
-always @ (negedge clk) begin
-	if (rst)
-		sp <= 32'h00004000;
-	else begin
-		if(UpdateSP)
-			sp <= NewSP;
-		if(WE)
-			if (Address >= 32'h00003000 && Address < 32'h00004000)
-				mem[((Address) & 32'h00000FFF) >> 2] <= StackWRdata;
-		else if (StackOp == 2'b10) begin  // push
-			mem[((sp_minus_4 & 32'h00000FFF)) >> 2] <= StackWRdata;
-			sp <= sp_minus_4;
-		end
-		else if (StackOp == 2'b11) // pop 
-			sp <= sp + 4;
-	end
+/*
+ * This always part controls the signal WriteReg_o.
+ */ 
+always @ (*) begin
+    if (rst)
+        WriteReg_o = 1'b0;
+    else
+        WriteReg_o = WriteReg_i;
+end
+
+/*
+ * This always part controls the signal MemData_o.
+ */ 
+always @ (*) begin
+    if (rst)
+        MemData_o = 32'b0;
+    else if (ALUop_i = 5'b10101)  // sw
+        MemData_o = Reg_i;
+end
+
+/*
+ * This always part controls the signal WriteDataAddr_o.
+ */ 
+always @ (*) begin
+    if (rst)
+        WriteDataAddr_o = 32'b0;
+    else begin
+        WriteDataAddr_o = WriteDataAddr_i;
+        if (ALUop_i = 5'b10100)  // lw
+            WriteDataAddr_o = MemData_i;
+    end
+end
+
+/*
+ * This always part controls the signal MemAddr_o.
+ */ 
+always @ (*) begin
+    if (rst)
+        MemAddr_o = 32'b0;
+    else begin
+        if (ALUop_i = 5'b1010x)  // lw or sw
+            MemAddr_o = MemAddr_i;
+        else
+            MemAddr_o = 32'b0;
+    end
+end
+
+/*
+ * This always part controls the signal MemCE_o.
+ */ 
+always @ (*) begin
+    if (rst)
+        MemCE_o = 1'b0;
+    else begin
+        if (ALUop_i = 5'b1010x)  // lw or sw
+            MemCE_o = 1'b1;
+        else
+            MemCE_o = 1'b0;
+    end
+end
+
+/*
+ * This always part controls the signal mem_we.
+ */ 
+always @ (*) begin
+    if (rst)
+        mem_we = 1'b0;
+    else begin
+        if (ALUop_i = 5'b10100)  // lw
+            mem_we = 1'b0;
+        else if (ALUop_i = 5'b10101)  // sw
+            mem_we = 1'b1;
+        else
+            mem_we = 1'b0;
+    end
 end
 
 endmodule
