52db16696fe620e67f008ee689a9b1f44dd91de2
Venci Freeman <37969638+VenciFreeman@users.noreply.github.com>
2019-12-27 20:49:07 +0800

    Add testbench.

--- TestCode/assembly.asm
+++ TestCode/assembly.asm
@@ -1,15 +1,15 @@
-LOOP:
-lw x1, 0(x0)
-lw x2, 4(x0)
-lw x3, 8(x0)
-lw x4, 12(x0)
-lw x5, 16(x0)
-lw x6, 20(x0)
-add x7, x1, x2
-sub x8, x3, x1
-or x9, x3, x1
-sll x10, x1, x2
-lw x11, 0(x0)
-addi x12, x10, 1
-jal x0,LOOP
-
+addi x1,x0,1  ; 00001 
+addi x2,x0,2  ; 00010
+addi x3,x0,16 ; 10000
+addi x4,x0,21 ; 10101
+addi x5,x0,31 ; 11111
+sub  x6,x2,x1 ; 00001
+sub  x7,x1,x2 ; 11111(-1)
+add  x8,x3,x2 ; 10010 
+add  x9,x1,x1 ; 00010
+and  x10,x1,x2; 00000
+and  x11,x4,x5; 10101
+or   x12,x1,x2; 00011
+or   x13,x4,x5; 11111
+xor  x14,x1,x2; 11100
+xor  x15,x4,x5; 00000
