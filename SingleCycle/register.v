24a148008675ace83a5eb6f2ddfc1992ed1978d2
Venci Freeman <37969638+VenciFreeman@users.noreply.github.com>
2019-12-28 16:33:43 +0800

    Finished.
    This file (register.v) is the registers of RISC-V CPU. You can read the comments to understand them.

--- SingleCycle/register.v
+++ SingleCycle/register.v
@@ -2,12 +2,13 @@
  * Ask me anything: via repo/issue, or e-mail: vencifreeman16@sjtu.edu.cn.
  * Author: @VenciFreeman (GitHub), copyright 2019.
  * School: Shanghai Jiao Tong University.
- *
+
  * Description:
- *
+ * This file is the registers of RISC-v CPU. 
+
  * Details:
- * - When we need to read readReg1, readReg2, read data from register file according to the
- *   address in readReg1, readReg2;
+ * - When we need to read readReg1, readReg2, read data from register file
+ *   according to the address in readReg1, readReg2;
  * - When we need to write rd, write data into register file according to the
  *   address in rd;
  * - The operation of writing data to the register file uses sequential logic, and
@@ -17,14 +18,15 @@
  * - If the read and write register signals are valid at the same time, and if the
  *   read address is the same as the write address, then the data which need to
  *   write can be directly output as read data to achieve data forwarding.
- *
+
  * History:
  * - 19/12/14: Create this file;
  * - 19/12/23: Modify the format;
- * - 19/12/26: Edit module. (I think it's finished.)
- *
+ * - 19/12/26: Edit module；
+ * - 19/12/28: Finished!
+
  * Notes:
- *
+ * - Single cycle donesn't need data forward.
  */
 
  module Registers(
@@ -34,8 +36,8 @@
     input   wire        we,
     input   wire[4:0]   WriteAddr,
     input   wire[31:0]  WriteData,
-    input   wire[1:0]   ReadReg1,
-    input   wire[1:0]   ReadReg2,
+    input   wire        ReadReg1,
+    input   wire        ReadReg2,
     input   wire[4:0]   ReadAddr1,
     input   wire[4:0]   ReadAddr2,
     output  reg [31:0]  ReadData1,
@@ -43,45 +45,39 @@
 
     );
 
-    reg [5:0]  n;
-    reg [31:0] regFile [31:0];
-    
+    reg [31:0] regFile [0:32];
+
 /*
-* This always part controls the signal reg_file, control write.
-*/    
+ * This always part controls the regFile, it's a 32*32 reg.
+ */    
 always @ (posedge clk) begin
-    if(!rst && we && WriteAddr != 5'h0) begin
-        regFile[WriteAddr] <= WriteData;
-        $display("x%d = %h", WriteAddr, WriteData);
+    regFile[5'h0] <= 32'b0;  // Register x0 always equals 0. 
+    if (!rst && we && WriteAddr != 5'h0) begin
+        regFile[WriteAddr] <= WriteData;  // Write data to register.
+        $display("x%d = %h", WriteAddr, WriteData);  // Display the change of register.
     end
 end
 
 /*
-* This always part controls the signal ReadData1, control read1.
-*/ 
+ * This always part controls the signal ReadData1 as rs1. 
+ */ 
 always @ (*) begin
     if(rst || ReadAddr1 == 5'h0)
         ReadData1 <= 32'b0;
-    else if (we && ReadReg1 && (ReadAddr1 == WriteAddr))
-        ReadData1 <= WriteData;
     else if (ReadReg1) begin
-        ReadData1 <= regFile[ReadReg1];
-        //$display("x%d = %h", ReadAddr1, ReadData1);
+        ReadData1 <= regFile[ReadAddr1];
     end else
         ReadData1 <= 32'b0;
 end
 
 /*
-* This always part controls the signal ReadData2, control read2.
-*/ 
+ * This always part controls the signal ReadData2 as rs2.
+ */ 
 always @ (*) begin
     if(rst || ReadAddr2 == 5'h0)
         ReadData2 <= 32'b0;
-    else if (we && ReadReg2 && (ReadAddr2 == WriteAddr))
-        ReadData2 <= WriteData;
     else if (ReadReg2) begin
-        ReadData2 <= regFile[ReadReg2];
-        //$display("x%d = %h", ReadAddr2, ReadData2);
+        ReadData2 <= regFile[ReadAddr2];
     end else
         ReadData2 <= 32'b0;
 end
