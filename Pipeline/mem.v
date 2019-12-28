4a28ccc7acaedaee097689f9c5c536e4ec0dcb08
Venci Freeman <37969638+VenciFreeman@users.noreply.github.com>
2019-12-29 02:30:36 +0800

    Finished.
    This file controls the read and write about data_mem.

--- Pipeline/mem.v
+++ Pipeline/mem.v
@@ -12,13 +12,15 @@
  * - Do no operation if there is other instruction.
 
  * History:
- * - 19/12/27: Create this pipeline file.
+ * - 19/12/27: Create this pipeline file;
+ * - 19/12/28: Fix some errors;
+ * - 19/12/29: Finished!
 
  * Notes:
  *
  */
 
-module MEM (
+module MEM(
 
     input   wire        rst,
     input   wire        WriteReg_i,
@@ -41,18 +43,14 @@ module MEM (
     reg mem_we;
     assign MemWE_o = mem_we;
 
-always @ (*) begin
-    $display("mem : data = %h addr = %d", WriteData_i, WriteDataAddr_i);
-end
-
 /*
  * This always part controls the signal WriteDataAddr_o.
  */ 
 always @ (*) begin
     if (rst)
-        WriteDataAddr_o = 5'b0;
+        WriteDataAddr_o <= 5'b0;
     else
-        WriteDataAddr_o = WriteDataAddr_i;
+        WriteDataAddr_o <= WriteDataAddr_i;
 end
 
 /*
@@ -60,9 +58,9 @@ end
  */ 
 always @ (*) begin
     if (rst)
-        WriteReg_o = 1'b0;
+        WriteReg_o <= 1'b0;
     else
-        WriteReg_o = WriteReg_i;
+        WriteReg_o <= WriteReg_i;
 end
 
 /*
@@ -70,9 +68,9 @@ end
  */ 
 always @ (*) begin
     if (rst)
-        MemData_o = 32'b0;
+        MemData_o <= 32'b0;
     else if (ALUop_i == 5'b10101)  // sw
-        MemData_o = Reg_i;
+        MemData_o <= Reg_i;
 end
 
 /*
@@ -80,11 +78,11 @@ end
  */ 
 always @ (*) begin
     if (rst)
-        WriteData_o = 32'b0;
+        WriteData_o <= 32'b0;
     else begin
-        WriteData_o = WriteData_i;
+        WriteData_o <= WriteData_i;
         if (ALUop_i == 5'b10100)  // lw
-            WriteData_o = MemData_i;
+            WriteData_o <= MemData_i;
     end
 end
 
@@ -93,12 +91,12 @@ end
  */ 
 always @ (*) begin
     if (rst)
-        MemAddr_o = 32'b0;
+        MemAddr_o <= 32'b0;
     else begin
         if (ALUop_i == 5'b1010x)  // lw or sw
-            MemAddr_o = MemAddr_i;
+            MemAddr_o <= MemAddr_i;
         else
-            MemAddr_o = 32'b0;
+            MemAddr_o <= 32'b0;
     end
 end
 
@@ -107,12 +105,12 @@ end
  */ 
 always @ (*) begin
     if (rst)
-        MemCE_o = 1'b0;
+        MemCE_o <= 1'b0;
     else begin
         if (ALUop_i == 5'b1010x)  // lw or sw
-            MemCE_o = 1'b1;
+            MemCE_o <= 1'b1;
         else
-            MemCE_o = 1'b0;
+            MemCE_o <= 1'b0;
     end
 end
 
@@ -121,14 +119,14 @@ end
  */ 
 always @ (*) begin
     if (rst)
-        mem_we = 1'b0;
+        mem_we <= 1'b0;
     else begin
         if (ALUop_i == 5'b10100)  // lw
-            mem_we = 1'b0;
+            mem_we <= 1'b0;
         else if (ALUop_i == 5'b10101)  // sw
-            mem_we = 1'b1;
+            mem_we <= 1'b1;
         else
-            mem_we = 1'b0;
+            mem_we <= 1'b0;
     end
 end
 
