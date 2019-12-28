6100a2b885dd285c198a374ee746a5da1d5852f1
Venci Freeman <37969638+VenciFreeman@users.noreply.github.com>
2019-12-29 02:12:42 +0800

    Finished.
    This file determines whether a conditional branch (jump) in the instruction flow of a program is likely to be taken or not.

--- Pipeline/bp.v
+++ Pipeline/bp.v
@@ -4,140 +4,169 @@
  * School: Shanghai Jiao Tong University.
 
  * Description:
- * Branch Predict.
+ * This file determines whether a conditional branch (jump) in the instruction
+ * flow of a program is likely to be taken or not.
 
  * Details:
+ * No details.
 
  * History:
- * - 19/12/27: Create this file.
+ * - 19/12/27: Create this file;
+ * - 19/12/28: Fix some errors;
+ * - 19/12/29: Finished!
 
  * Notes:
-
  */
 
-module branch_pre(
+module BRANCH_PRE(
 
 	input   wire        rst,
-	//from if
 	input   wire[31:0]  pc_i,
 	input   wire[31:0]	inst_i,
-	//from id
-	input   wire		id_is_branch,
-	input   wire		id_take_or_not,
-	input   wire		id_pre_true,
-	input   wire		id_sel,
-	input   wire[31:0] 	id_pc,											
-	//to pc_reg
-	output  reg         pre_branch_flag_o,
-	output  reg [31:0]  pre_branch_target_address_o,
-	//to id
-	output  reg			pre_take_or_not,
-	output  reg			pre_sel
+	input   wire		Branch,
+	input   wire		Accept,
+	input   wire		Predict,
+	input   wire		idSel,
+	input   wire[31:0] 	idPC,											
+	output  reg         PreBranch,
+	output  reg [31:0]  PreAddr,
+	output  reg			PreAccept,
+	output  reg			PreSel
 		
 );
 
-	reg [0:9]   overall_addr_rec[0:4095];
-	reg [1:0]	overall_pre[0:1023];
+	reg [0:9]   AllAddrRec[0:4095];
+	reg [1:0]	AllPre[0:1023];
 	
-	reg [11:0] 	global_rec;
-	reg [1:0] 	global_pre[0:4095];
+	reg [11:0] 	GlobalRec;
+	reg [1:0] 	GlobalPre[0:4095];
 	
-	reg [0:9] 	local_addr_rec[0:1023];
-	reg [1:0]	local_pre[0:1023];
+	reg [0:9] 	LocalAddrRec[0:1023];
+	reg [1:0]	LocalPre[0:1023];
 	
 	integer i;
 
+/*
+ * This always part controls the signal AllAddrRec.
+ */
 always @ (*) begin
     if (rst)
         for(i = 0; i < 4096; i = i + 1)
-				overall_addr_rec[i] = 10'b0;
+			AllAddrRec[i] = 10'b0;
 end
 
+/*
+ * This always part controls the signal LocalAddrRec.
+ */
 always @ (*) begin
     if (rst)
         for(i = 0; i < 1024; i = i + 1)
-			local_addr_rec[i] <= 10'b0;
+			LocalAddrRec[i] <= 10'b0;
 end
 
+/*
+ * This always part controls the signal GlobalRec.
+ */
+always @ (*) begin
+    if (rst)
+        GlobalRec <= 1'b0;
+    else
+        GlobalRec <= (GlobalRec << 1 | Accept);
+end
+
+/*
+ * This always part controls the signal GlobalPre.
+ */
 always @ (*) begin
     if (rst)
         for(i = 0; i < 4096; i = i + 1)
-			global_pre[i] = 2'b0;
-    else if (id_is_branch && id_pre_true && id_sel && global_pre[global_rec] < 3)
-        global_pre[global_rec] = global_pre[global_rec] + 1;
-    else if (id_is_branch && !id_pre_true && id_sel && global_pre[global_rec] > 0)
-        global_pre[global_rec] = global_pre[global_rec] - 1;
-    global_rec <= (global_rec << 1 | id_take_or_not);
+			GlobalPre[i] = 2'b0;
+    else if ( Branch && Predict && idSel && (GlobalPre[GlobalRec] < 3) )
+        GlobalPre[GlobalRec] = GlobalPre[GlobalRec] + 1;
+    else if ( Branch && !Predict && idSel && (GlobalPre[GlobalRec] > 0) )
+        GlobalPre[GlobalRec] = GlobalPre[GlobalRec] - 1;
 end
 
+/*
+ * This always part controls the signal LocalPre.
+ */
 always @ (*) begin
     if (rst)
         for(i = 0; i < 1024; i = i + 1)
-			local_pre[i] <= 2'b0;
-    else if (id_is_branch && id_pre_true && !id_sel && local_pre[local_addr_rec[id_pc[11:2]]] < 3)
-        local_pre[local_addr_rec[id_pc[11:2]]] <= local_pre[local_addr_rec[id_pc[11:2]]] + 1;
-    else if (id_is_branch && !id_pre_true && !id_sel && local_pre[local_addr_rec[id_pc[11:2]]] > 0)
-        local_pre[local_addr_rec[id_pc[11:2]]] <= local_pre[local_addr_rec[id_pc[11:2]]] - 1;
+			LocalPre[i] <= 2'b0;
+    else if ( Branch && Predict && !idSel && (LocalPre[LocalAddrRec[idPC[11:2]]] < 3) )
+        LocalPre[LocalAddrRec[idPC[11:2]]] <= LocalPre[LocalAddrRec[idPC[11:2]]] + 1;
+    else if ( Branch && !Predict && !idSel && (LocalPre[LocalAddrRec[idPC[11:2]]] > 0) )
+        LocalPre[LocalAddrRec[idPC[11:2]]] <= LocalPre[LocalAddrRec[idPC[11:2]]] - 1;
 end
 
+/*
+ * This always part controls the signal PreBranch.
+ */
 always @ (*) begin
     if (rst)
-        global_pre = 12'b0;
-end
-
-always @ (*) begin
-    if (rst)
-        pre_branch_flag_o <= 1'b0;
+        PreBranch <= 1'b0;
     else if (inst_i[6:0] == 7'b1100011)
-        pre_branch_flag_o <= 1'b1;
+        PreBranch <= 1'b1;
 end
 
+/*
+ * This always part controls the signal PreAddr.
+ */
 always @ (*) begin
     if (rst)
-        pre_branch_target_address_o <= 32'b0;
-    else if (inst_i[6:0] == 7'b1100011)
-        if (!overall_pre[overall_addr_rec[pc_i[13:2]]][1]) begin
-            if (!local_pre[local_addr_rec[pc_i[11:2]]][1])
-                pre_branch_target_address_o <= pc_i + 4;
+        PreAddr <= 32'b0;
+    else if (inst_i[6:0] == 7'b1100011) begin
+        if ( AllPre[AllAddrRec[pc_i[13:2]]][1] ) begin
+            if ( GlobalPre[GlobalRec][1] )
+                PreAddr <= pc_i + {{20{inst_i[31]}}, inst_i[7], inst_i[30:25], inst_i[11:8], 1'b0};
             else
-                pre_branch_target_address_o <= pc_i + {{20{inst_i[31]}}, inst_i[7], inst_i[30:25], inst_i[11:8], 1'b0};
+                PreAddr <= pc_i + 4;
         end
         else begin
-            if (global_pre[global_rec][1] == 0)
-                pre_branch_target_address_o <= pc_i + 4;
+            if ( LocalPre[LocalAddrRec[pc_i[11:2]]][1] )
+                PreAddr <= pc_i + {{20{inst_i[31]}}, inst_i[7], inst_i[30:25], inst_i[11:8], 1'b0};
             else
-                pre_branch_target_address_o <= pc_i + {{20{inst_i[31]}}, inst_i[7], inst_i[30:25], inst_i[11:8], 1'b0};
+                PreAddr <= pc_i + 4;
         end
+    end
 end
 
+/*
+ * This always part controls the signal PreAccept.
+ */
 always @ (*) begin
     if (rst)
-        pre_take_or_not <= 1'b0;
-    else if (inst_i[6:0] == 7'b1100011)
-        if (!overall_pre[overall_addr_rec[pc_i[13:2]]][1]) begin
-            if (!local_pre[local_addr_rec[pc_i[11:2]]][1])
-                pre_take_or_not <= 1'b0;
+        PreAccept <= 1'b0;
+    else if (inst_i[6:0] == 7'b1100011) begin
+        if ( AllPre[AllAddrRec[pc_i[13:2]]][1] ) begin
+            if (GlobalPre[GlobalRec][1])
+                PreAccept <= 1'b1;
             else
-                pre_take_or_not <= 1'b1;
+                PreAccept <= 1'b0;
         end
         else begin
-            if (global_pre[global_rec][1] == 0)
-                pre_take_or_not <= 1'b0;
+            if ( LocalPre[LocalAddrRec[pc_i[11:2]]][1] )
+                PreAccept <= 1'b1;
             else
-                pre_take_or_not <= 1'b1;
+                PreAccept <= 1'b0;
         end
+    end
 end
 
-
+/*
+ * This always part controls the signal PreASel.
+ */
 always @ (*) begin
 	if (rst) 
-		pre_sel <= 1'b0;
+		PreSel <= 1'b0;
 	else if (inst_i[6:0] == 7'b1100011) 
-		if (overall_pre[overall_addr_rec[pc_i[13:2]]][1] == 0)
-			pre_sel <= 1'b0;
+		if (AllPre[AllAddrRec[pc_i[13:2]]][1])
+			PreSel <= 1'b1;
         else
-			pre_sel <= 1'b1;
-    	else pre_branch_flag_o <= 1'b0;
+			PreSel <= 1'b0;
+    else
+        PreSel <= 1'b0;
 end
 
 
